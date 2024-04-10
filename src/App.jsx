03092bf7c6b4841c673ba8d9099f4c8ccdf5ea0a
Allan Rubio <allanrubio@MacBook-Pro.lan>
2024-04-09 23:54:23 -0700

    added working button that takes you back to contact list

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ export default function App() {
         {selectedContactId ? (
                               <SelectedContact 
                               selectedContactId={selectedContactId}
-                              setselectedContactId={selectedContactId}/>
+                              setSelectedContactId={setSelectedContactId}
+                              />
                              ):(
                               <ContactList setSelectedContactId={setSelectedContactId} />)}
         
--- src/components/SelectedContact.jsx
+++ src/components/SelectedContact.jsx
@@ -1,68 +1,50 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-const fetchAPI = "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/"
+const fetchAPI =
+  "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users/";
 
-export default function SelectedContact({selectedContactId, setselectedContactId}){
+export default function SelectedContact({
+  selectedContactId,
+  setSelectedContactId,
+}) {
+  const [contact, setContact] = useState({});
 
-    const [contact, setContact] = useState({});
+  useEffect(() => {
+    async function fetchSingleContact() {
+      try {
+        const response = await fetch(`${fetchAPI}${selectedContactId}`);
+        const result = await response.json();
 
-    useEffect(() => {
-        async function fetchSingleContact() {
-          try {
-            const response = await fetch(`${fetchAPI}${selectedContactId}`);
-            const result = await response.json();
-    
-            setContact(result);
-
-          } catch (error) {
-            console.error(error);
-          }
-        }
-        fetchSingleContact();
-      }, []);
-
-      return (
-        <table>
-          <thead>
-            <tr>
-              <th colSpan="3">Contact List</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{contact.name}</td>
-              <td>{contact.email}</td>
-              <td>{contact.phone}</td>
-            </tr>
-
-          </tbody>
-        </table>
-      );
-
-}
-
-/*
-{
-  "id": 1,
-  "name": "Mrs. Jerald Schulist",
-  "username": "Antwan",
-  "email": "Coby_Zieme@libby.tv",
-  "address": {
-    "street": "Morissette Heights",
-    "suite": "Apt. 633",
-    "city": "Port Liashire",
-    "zipcode": "04270",
-    "geo": {
-      "lat": "27.7501",
-      "lng": "-83.1776"
+        setContact(result);
+        console.log(contact);
+      } catch (error) {
+        console.error(error);
+      }
     }
-  },
-  "phone": "(663)839-3814 x845",
-  "website": "arvel.io",
-  "company": {
-    "name": "D'Amore-Krajcik",
-    "catchPhrase": "Down-sized empowering Graphic Interface",
-    "bs": "extend cross-media relationships"
-  }
+    fetchSingleContact();
+  }, [selectedContactId]); //By including selectedContactId in the dependency array, it ensures that the effect runs whenever the selectedContactId changes, allowing the component to fetch the correct contact details for the selected contact and update accordingly.
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="3">Contact Information</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Name: {contact.name}</td>
+            <td> Email: {contact.email}</td>
+            <td> Phone: {contact.phone}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <button onClick={() => setSelectedContactId(null)}>
+        Go Back to Contact List
+      </button>
+    </>
+  );
 }
-*/
